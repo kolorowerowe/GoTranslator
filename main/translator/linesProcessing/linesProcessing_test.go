21fa3e3bad32b2457fbb061dfc0a5594ac04dd02
kolorowerowe <34072563+kolorowerowe@users.noreply.github.com>
2020-06-03 00:42:24 +0200

    one more test case

--- main/translator/linesProcessing/linesProcessing_test.go
+++ main/translator/linesProcessing/linesProcessing_test.go
@@ -13,14 +13,18 @@ func Test_addLinks(t *testing.T) {
 		input: []string{
 			"[I'm an inline-style link](https://www.google.com)",
 			"[I'm a relative reference to a repository file](../blob/master/LICENSE)",
+			"[I'm a reference link][1]",
 		},
 		wanted: []string{
 			`<a href="https://www.google.com">I'm an inline-style link</a>`,
 			`<a href="../blob/master/LICENSE">I'm a relative reference to a repository file</a>`,
+			`<a href="link1" title="title1">I'm a reference link</a>`,
 		},
 	}
 
 	referencesMap := make(map[string]ReferencesInfo)
+	referencesMap["1"] = ReferencesInfo{link: "link1", title: "title1"}
+
 	got := AddLinks(lineTests.input, referencesMap)
 
 	if len(got) != len(lineTests.wanted) {
